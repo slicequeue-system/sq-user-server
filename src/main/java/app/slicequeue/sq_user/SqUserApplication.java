52bb5924ca5e658309bc9c83d6b5f85fbf68de67
slicequeue <slicequeue@gmail.com>
2025-05-11 02:17:31 +0900

    setting: 최초 커밋
    - sq 아티팩트 연결 및 필요 라이브러리 추가

---
+++ src/main/java/app/slicequeue/sq_user/SqUserApplication.java
@@ -0,0 +1,13 @@
+package app.slicequeue.sq_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqUserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/slicequeue/sq_user/SqUserApplicationTests.java
@@ -0,0 +1,13 @@
+package app.slicequeue.sq_user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
