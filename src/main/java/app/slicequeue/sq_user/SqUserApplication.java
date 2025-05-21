4b015635040e030663812e51a66d03a22df67776
slicequeue <slicequeue@gmail.com>
2025-05-21 23:28:52 +0900

    chore: 엔티티, 리포지토리 스캔 경로 지정 - sq-common-message-relay-starter 준비

--- src/main/java/app/slicequeue/sq_user/SqUserApplication.java
+++ src/main/java/app/slicequeue/sq_user/SqUserApplication.java
@@ -2,9 +2,19 @@ package app.slicequeue.sq_user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = {
+		"app.slicequeue.sq_user.user.command.infra",
+		"app.slicequeue.common.base.messagerelay.domain"
+})
+@EntityScan(basePackages = {
+		"app.slicequeue.sq_user.user.command.domain",
+		"app.slicequeue.common.base.messagerelay.domain"
+})
 public class SqUserApplication {
 
 	public static void main(String[] args) {
